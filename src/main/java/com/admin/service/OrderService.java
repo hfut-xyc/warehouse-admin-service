62ee0141f9d52d61f15d1b8889e533188de51561
hfut-xyc <hfut.xieyincen@foxmail.com>
2025-01-26 10:40:06 +0800

    重写下订单逻辑

--- src/main/java/com/admin/service/OrderService.java
+++ src/main/java/com/admin/service/OrderService.java
@@ -16,7 +16,6 @@ import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Service
@@ -34,10 +33,7 @@ public class OrderService {
     @Resource
     private WarehouseProductMapper warehouseProductMapper;
 
-    private final ReentrantLock lock = new ReentrantLock();
-
-    private final AtomicBoolean atomicFlag = new AtomicBoolean(false);
-    private final Object object = new Object();
+    private final ReentrantLock lockInsertStock = new ReentrantLock();
 
     /**
      * 根据订单日期分页查询订单
@@ -54,7 +50,6 @@ public class OrderService {
     }
 
 
-    @Transactional
     public Integer updateStockOrder(Order order) throws Exception {
         Warehouse warehouse = warehouseMapper.selectByWid(order.getWarehouseId());
         if (warehouse == null) {
@@ -64,12 +59,13 @@ public class OrderService {
         if (product == null) {
             throw new Exception("商品不存在");
         }
-        // 事务提交后，再释放锁
-        try {
-            lock.lock();
-            updateStock(order);
-        } finally {
-            lock.unlock();
+
+        String str = "" + warehouse.getWarehouseId() + product.getProductId();
+        synchronized (str.intern()) {
+            // 先提交事务，再释放锁
+            //OrderService proxy = (OrderService) AopContext.currentProxy();
+            //proxy.updateStock(order);
+            this.updateStock(order);
         }
         return 1;
     }
@@ -80,7 +76,6 @@ public class OrderService {
         if (record == null) {
             throw new Exception("库存不存在");
         }
-        // 有库存记录，直接更新库存
         if (record.getStock() + order.getCount() < 0) {
             throw new Exception("库存不足");
         }
@@ -98,7 +93,6 @@ public class OrderService {
         }
     }
 
-    @Transactional
     public Integer insertStockOrder(Order order) throws Exception {
         if (order.getCount() <= 0) {
             throw new Exception("插入的库存数量必须大于0");
@@ -112,40 +106,45 @@ public class OrderService {
             throw new Exception("商品不存在");
         }
 
+        try {
+            lockInsertStock.lock();
+            //OrderService proxy = (OrderService) AopContext.currentProxy();
+            //proxy.insertStock(order);
+            this.insertStock(order);
+        } finally {
+            // 先提交事务，再释放锁
+            lockInsertStock.unlock();
+        }
+
+        return 1;
+    }
+
+    @Transactional
+    public void insertStock(Order order) throws Exception {
         WarehouseProduct warehouseProduct = new WarehouseProduct();
         warehouseProduct.setWarehouseId(order.getWarehouseId());
         warehouseProduct.setProductId(order.getProductId());
         warehouseProduct.setWarehouseName(order.getWarehouseName());
         warehouseProduct.setProductName(order.getProductName());
 
-        if (atomicFlag.compareAndSet(false, true)) {
-            WarehouseProduct record = warehouseProductMapper.selectByWidPid(order.getWarehouseId(), order.getProductId());
-            if (record != null) {
-                throw new Exception("库存已存在");
-            }
+        WarehouseProduct record = warehouseProductMapper.selectByWidPid(order.getWarehouseId(), order.getProductId());
+        if (record == null) {
             warehouseProduct.setStock(order.getCount());
             Integer res = warehouseProductMapper.insert(warehouseProduct);
             if (res != 1) {
                 throw new Exception("插入库存失败");
             }
         } else {
-            Thread.yield();
-            Thread.sleep(500);
-            WarehouseProduct record = warehouseProductMapper.selectByWidPid(order.getWarehouseId(), order.getProductId());
-            if (record == null) {
-                throw new Exception("库存不存在");
-            }
             warehouseProduct.setStock(record.getStock() + order.getCount());
             Integer res = warehouseProductMapper.updateStock(warehouseProduct);
             if (res != 1) {
                 throw new Exception("更新库存失败");
             }
         }
-        Integer res1 = orderMapper.insert(order);
-        if (res1 != 1) {
+        Integer res = orderMapper.insert(order);
+        if (res != 1) {
             throw new Exception("添加订单失败");
         }
-        return 1;
     }
 
     @Transactional
